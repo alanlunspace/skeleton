c8fcdd027a64f8e10d52ba83a178555eb7e42b1a
alanlunspace <alanlunspace@gmail.com>
2019-09-13 17:50:26 +0800

    Added python/lib/log.py

---
+++ python/lib/log.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import json
+import os
+import logging
+import logging.config
+
+log_config_filepath = open('config/logging_conf.json')
+log_config_json = json.load(log_config_filepath)
+log_filename = log_config_json['handlers']['file']['filename']
+log_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), os.path.dirname(log_filename))
+
+if not os.path.exists(log_dir):
+    try:
+        os.makedirs(log_dir)
+    except OSError as e:
+        raise e
+
+logging.config.dictConfig(log_config_json)
+log = logging.getLogger(__name__)
+
+# log.debug(__file__)
--- python/python.py
+++ python/python.py
@@ -1,23 +1,8 @@
 #!/usr/bin/env python3
-import json
+from lib.log import log
 import os
-import logging
-import logging.config
-import coloredlogs
-import fileinput
 
-log_config_filepath = open('config/logging_conf.json')
-log_config_json = json.load(log_config_filepath)
-log_filename = log_config_json['handlers']['file']['filename']
-log_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), os.path.dirname(log_filename))
+basename = os.path.basename(__file__)
+dirname = os.path.dirname(basename)
 
-if not os.path.exists(log_dir):
-    try:
-        os.makedirs(log_dir)
-    except OSError as e:
-        raise e
-
-logging.config.dictConfig(log_config_json)
-logger = logging.getLogger(__name__)
-
-logger.debug(f"Hellow World!")
+log.debug(__file__)
