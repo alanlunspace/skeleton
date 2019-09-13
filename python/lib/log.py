d86e1dfce9c167d2ec95d01a91dc254acc927d31
alanlunspace <alanlunspace@gmail.com>
2019-09-13 18:02:30 +0800

    Fixed create log file and directory if missing

--- python/lib/log.py
+++ python/lib/log.py
@@ -7,13 +7,16 @@ import logging.config
 log_config_filepath = open('config/logging_conf.json')
 log_config_json = json.load(log_config_filepath)
 log_filename = log_config_json['handlers']['file']['filename']
-log_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), os.path.dirname(log_filename))
+log_filepath = os.path.join(os.path.dirname(os.path.realpath(__file__)), log_filename)
+log_dir = os.path.dirname(log_filename)
 
 if not os.path.exists(log_dir):
     try:
         os.makedirs(log_dir)
     except OSError as e:
         raise e
+
+open(log_filename, 'w')
 
 logging.config.dictConfig(log_config_json)
 log = logging.getLogger(__name__)
